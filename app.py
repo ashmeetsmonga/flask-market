24a515c330cad4b5ae29d4d4493e14a94f0deb05
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-21 22:54:30 +0530

    added mongodb

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from config import MONGO_URI
 app = Flask(__name__)
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app)
 
 PORT = 5000
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return '<h1>Hello World</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=PORT)
