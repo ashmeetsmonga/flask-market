066b1e44624e88b56b4a37bffebce1f2081c2ed1
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-03-22 21:39:14 +0530

    button added

--- client/src/components/Market.jsx
+++ client/src/components/Market.jsx
@@ -13,7 +13,7 @@ const Market = () => {
 	}
 
 	return (
-		<div className='container'>
+		<div className='container mt-4'>
 			<h2>Market</h2>
 			{isLoading && <h4>Loading...</h4>}
 			{data && (
@@ -24,6 +24,7 @@ const Market = () => {
 							<th scope='col'>Name</th>
 							<th scope='col'>Price</th>
 							<th scope='col'>Barcode</th>
+							<th scope='col'>Options</th>
 						</tr>
 					</thead>
 					<tbody>
@@ -33,6 +34,14 @@ const Market = () => {
 								<td>{item.name}</td>
 								<td>{item.price}</td>
 								<td>{item.barcode}</td>
+								<td className='d-flex gap-2'>
+									<button type='button' className='btn btn-info'>
+										More Info
+									</button>
+									<button type='button' className='btn btn-success'>
+										Purchase Item
+									</button>
+								</td>
 							</tr>
 						))}
 					</tbody>
